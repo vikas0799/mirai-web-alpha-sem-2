52c0340229b4beaff5dd15ca696be1882cd4cfa3
Vikas Patel <mirai@Johns-MacBook-Air.local>
2026-06-03 09:54:04 +0530

    modules

---
+++ Lect-23-nodejs-modules/Auth.js
@@ -0,0 +1,15 @@
+let login=true;
+console.log(login);
+let dipesh="good student";
+
+function Auth(){
+    console.log("login success");
+    
+}
+
+module.exports={login,Auth}
+
+//....
+
+
+
---
+++ Lect-23-nodejs-modules/main.js
@@ -0,0 +1,23 @@
+console.log("hi running");
+
+
+// console.log(login);
+
+// let featureX=require("./Auth.js");
+let {logn,Auth}=require("./Auth.js");
+// let login=true;
+// console.log(login);
+
+
+// function Auth(){
+//     console.log("login success");
+    
+// }
+// console.log(featureX);
+// featureX.Auth();
+Auth();
+
+let dipeshMacOS=require("os");
+console.log(dipeshMacOS.cpus());
+
+
